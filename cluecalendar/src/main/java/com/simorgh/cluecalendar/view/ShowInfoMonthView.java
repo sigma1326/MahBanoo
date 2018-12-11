c3e0a93849fae2eb68187907c95409bef23cfa2c
sigma <taleb.zarhesh@gmail.com>
2018-12-11 09:50:37 +0330

    bug fix

--- cluecalendar/src/main/java/com/simorgh/cluecalendar/view/ShowInfoMonthView.java
+++ cluecalendar/src/main/java/com/simorgh/cluecalendar/view/ShowInfoMonthView.java
@@ -33,7 +33,6 @@ public class ShowInfoMonthView extends BaseMonthView {
     //rectangle colors
     private Paint rectTypeRedPaint;
     private Paint rectTypeGreenPaint;
-    private Paint rectTypeGreen2Paint;
     private Paint rectTypeYellowPaint;
     private Paint rectTypeMarkedPaint;
     private int rectTypeRedColor;
@@ -47,146 +46,6 @@ public class ShowInfoMonthView extends BaseMonthView {
 
     private static Bitmap icon;
     private IsDayMarkedListener isDayMarkedListener;
-
-
-    public static class ClueData {
-        private static final int DEFAULT_RED_COUNT = 4;
-        private static final int DEFAULT_GRAY_COUNT = 24;
-        private static final int DEFAULT_GREEN_COUNT = 3;
-        private static final int DEFAULT_YELLOW_COUNT = 2;
-        private static final int DEFAULT_GREEN2_INDEX = 7;
-
-        private int redCount = DEFAULT_RED_COUNT;
-        private int grayCount = DEFAULT_GRAY_COUNT;
-        private int greenStartIndex = DEFAULT_GREEN2_INDEX - 1;
-        private int greenEndIndex = DEFAULT_GREEN2_INDEX + 1;
-        private int yellowCount = DEFAULT_YELLOW_COUNT;
-        private int yellowStartIndex = -1;
-        private int yellowEndIndex = -1;
-        private int green2Index = DEFAULT_GREEN2_INDEX;
-        private int totalDays = 0;
-
-        public ClueData(int redCount, int grayCount, int yellowCount) {
-            this.redCount = redCount;
-            this.grayCount = grayCount;
-            this.yellowCount = yellowCount;
-            this.totalDays = redCount + grayCount;
-            switch (grayCount) {
-                case 21:
-                    green2Index = redCount + 7;
-                    break;
-                case 22:
-                    green2Index = redCount + 8;
-                    break;
-                case 23:
-                    green2Index = redCount + 9;
-                    break;
-                case 24:
-                    green2Index = redCount + 10;
-                    break;
-                case 25:
-                    green2Index = redCount + 11;
-                    break;
-                case 26:
-                    green2Index = redCount + 12;
-                    break;
-                case 27:
-                    green2Index = redCount + 13;
-                    break;
-                case 28:
-                    green2Index = redCount + 14;
-                    break;
-                default:
-                    Log.d(TAG, "ClueData: invalid cycle length ");
-            }
-            greenStartIndex = green2Index - 1;
-            greenEndIndex = green2Index + 1;
-            yellowStartIndex = totalDays - yellowCount + 1;
-            yellowEndIndex = totalDays;
-        }
-
-        public ClueData(int redCount, int grayCount) {
-            this.redCount = redCount;
-            this.grayCount = grayCount;
-            this.yellowCount = DEFAULT_YELLOW_COUNT;
-            this.totalDays = redCount + grayCount;
-            switch (grayCount) {
-                case 21:
-                    green2Index = redCount + 7;
-                    break;
-                case 22:
-                    green2Index = redCount + 8;
-                    break;
-                case 23:
-                    green2Index = redCount + 9;
-                    break;
-                case 24:
-                    green2Index = redCount + 10;
-                    break;
-                case 25:
-                    green2Index = redCount + 11;
-                    break;
-                case 26:
-                    green2Index = redCount + 12;
-                    break;
-                case 27:
-                    green2Index = redCount + 13;
-                    break;
-                case 28:
-                    green2Index = redCount + 14;
-                    break;
-                default:
-                    Log.d(TAG, "ClueData: invalid cycle length ");
-            }
-            greenStartIndex = green2Index - 1;
-            greenEndIndex = green2Index + 1;
-            yellowStartIndex = totalDays - yellowCount + 1;
-            yellowEndIndex = totalDays;
-        }
-
-        public ClueData() {
-        }
-
-        public int getRedCount() {
-            return redCount;
-        }
-
-        public void setRedCount(int redCount) {
-            this.redCount = redCount;
-        }
-
-        public int getGrayCount() {
-            return grayCount;
-        }
-
-        public void setGrayCount(int grayCount) {
-            this.grayCount = grayCount;
-        }
-
-        public int getYellowCount() {
-            return yellowCount;
-        }
-
-        public void setYellowCount(int yellowCount) {
-            this.yellowCount = yellowCount;
-        }
-
-        public int getGreen2Index() {
-            return green2Index;
-        }
-
-        public void setGreen2Index(int green2Index) {
-            this.green2Index = green2Index;
-        }
-
-        public int getTotalDays() {
-            return totalDays;
-        }
-
-        public void setTotalDays(int totalDays) {
-            this.totalDays = totalDays;
-        }
-    }
 
     public ShowInfoMonthView(Context context) {
         super(context);
@@ -267,11 +126,6 @@ public class ShowInfoMonthView extends BaseMonthView {
         rectTypeGreenPaint.setAntiAlias(true);
         rectTypeGreenPaint.setStyle(Paint.Style.FILL);
         rectTypeGreenPaint.setColor(rectTypeGreenColor);
-
-        rectTypeGreen2Paint = new Paint();
-        rectTypeGreen2Paint.setAntiAlias(true);
-        rectTypeGreen2Paint.setStyle(Paint.Style.FILL);
-        rectTypeGreen2Paint.setColor(rectTypeGreen2Color);
 
         rectTypeYellowPaint = new Paint();
         rectTypeYellowPaint.setAntiAlias(true);
