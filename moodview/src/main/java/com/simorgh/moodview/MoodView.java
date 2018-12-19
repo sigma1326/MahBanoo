af443a06fd45a16b47c40c9a43a1deb9e63ca345
sigma <taleb.zarhesh@gmail.com>
2018-12-19 11:23:05 +0330

    init MoodView Class

---
+++ moodview/src/main/java/com/simorgh/moodview/MoodView.java
@@ -0,0 +1,28 @@
+package com.simorgh.moodview;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class MoodView extends View {
+    public MoodView(Context context) {
+        super(context);
+    }
+
+    public MoodView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MoodView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public MoodView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
