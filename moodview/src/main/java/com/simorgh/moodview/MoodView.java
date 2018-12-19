a9ef8711eee0cd10164fefe9beb5810e2b7c1b33
sigma <taleb.zarhesh@gmail.com>
2018-12-19 15:59:19 +0330

    update MoodView

--- moodview/src/main/java/com/simorgh/moodview/MoodView.java
+++ moodview/src/main/java/com/simorgh/moodview/MoodView.java
@@ -24,5 +24,6 @@ public class MoodView extends View {
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public MoodView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+
     }
 }
